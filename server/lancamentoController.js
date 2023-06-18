d8558005bfcb2c1697d47a23a84a7d0ae27a6e0c
heitormariano <heitormariano.camara@gmail.com>
2023-06-17 22:46:22 -0300

    criacao de metodos auxiliares para serem usados nas operacoes get, post e delete registrados

--- server/lancamentoController.js
+++ server/lancamentoController.js
@@ -2,43 +2,53 @@ const LancamentoDto = require('./lancamentoDto');
 
 class LancamentoController {
   constructor(httpServer, lancamentoDao) {
-    // analisar o uso de callbacks (melhorar a passagem do terceiro argumento)
-    httpServer.register('get', '/api/lancamentos', function (req, res) {
-      lancamentoDao.getLancamentos((erro, dados) => {
-        if (erro) throw erro;
-        const lancamentos = [];
-        for (const lancamentoBanco of dados) {
-          lancamentos.push(
-            new LancamentoDto(
-              lancamentoBanco.id_lancamento,
-              lancamentoBanco.mes,
-              lancamentoBanco.categoria,
-              lancamentoBanco.tipo,
-              parseFloat(lancamentoBanco.valor)
-            )
-          );
-        }
-        res.json(lancamentos);
-      });
+    // Se possível, melhorar a passagem dos callbacks das operações (get, post, delete)
+    httpServer.register('get', '/api/lancamentos', (req, res) => {
+      this.getLancamentosHandler(req, res, lancamentoDao);
     });
 
-    // analisar o uso de callbacks
-    httpServer.register('post', '/api/lancamentos', function (req, res) {
-      const lancamento = req.body;
-      lancamentoDao.saveLancamentos(lancamento, function (erro) {
-        if (erro) throw erro;
-        res.end();
-      });
+    httpServer.register('post', '/api/lancamentos', (req, res) => {
+      this.saveLancamentosHandler(req, res, lancamentoDao);
     });
 
-    // Metodo/funcionalidade testada via Postman também
-    // metodo DELETE. URL exemplo: http://localhost:3000/api/lancamentos/21
-    httpServer.register('delete', '/api/lancamentos/:idLancamento', function (req, res) {
-      const idLancamento = req.params.idLancamento;
-      lancamentoDao.deleteLancamentos(idLancamento, function (erro) {
-        if (erro) throw erro;
-        res.end();
-      });
+    // Postman - URL exemplo: http://localhost:3000/api/lancamentos/21
+    httpServer.register('delete', '/api/lancamentos/:idLancamento', (req, res) => {
+      this.deleteLancamentosHandler(req, res, lancamentoDao);
+    });
+  }
+
+  getLancamentosHandler(req, res, lancamentoDao) {
+    lancamentoDao.getLancamentos((erro, dados) => {
+      if (erro) throw erro;
+      const lancamentos = [];
+      for (const lancamentoBanco of dados) {
+        lancamentos.push(
+          new LancamentoDto(
+            lancamentoBanco.id_lancamento,
+            lancamentoBanco.mes,
+            lancamentoBanco.categoria,
+            lancamentoBanco.tipo,
+            parseFloat(lancamentoBanco.valor)
+          )
+        );
+      }
+      res.json(lancamentos);
+    });
+  }
+
+  saveLancamentosHandler(req, res, lancamentoDao) {
+    const lancamento = req.body;
+    lancamentoDao.saveLancamentos(lancamento, function (erro) {
+      if (erro) throw erro;
+      res.end();
+    });
+  }
+
+  deleteLancamentosHandler(req, res, lancamentoDao) {
+    const idLancamento = req.params.idLancamento;
+    lancamentoDao.deleteLancamentos(idLancamento, function (erro) {
+      if (erro) throw erro;
+      res.end();
     });
   }
 }
